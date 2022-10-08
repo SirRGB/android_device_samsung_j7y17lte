0f515e7773c70ef4042575981e7343aa93e145c7
Alejandro <apamarin@gmail.com>
2022-10-09 00:19:17 +0200

    j7y17lte: depricate WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,6 @@ WIFI_DRIVER_NVRAM_PATH           := "/vendor/etc/wifi/nvram_net.txt"
 WIFI_DRIVER_FW_PATH_STA          := "/vendor/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BAND                        := 802_11_ABG
-WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true
 
 # inherit from common
 -include device/samsung/universal7870-common/BoardConfigCommon.mk
